0b888b85a059b30af0843207ff08bb9c562b5b08
Fikri Maulana Ibrahim <fikri.ibrahim@ist.id>
2022-09-19 23:43:26 +0700

    integration at menu dashboard dan courses 20%

--- src/containers/Courses/index.tsx
+++ src/containers/Courses/index.tsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import {
+  Divider,
+  Grid,
+  Paper,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { studentActions } from "../../store/student-slice";
+import { doGetCourses } from "../Dashboard/ApiServiceDashboard";
+
+const styles = {
+  root: { padding: "30px 20px" },
+  title: {
+    fontWeight: 500,
+    fontSize: 24,
+    color: "#202020",
+  },
+  paper: {
+    display: "flex",
+    padding: "20px",
+    alignItems: "center",
+    borderRadius: "10px",
+    position: "relative",
+  },
+};
 
 const Courses = () => {
-  return (
-    <div>Courses</div>
-  )
-}
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
+  const dispatch = useAppDispatch();
+  const studentId = useAppSelector((state) => state.student.studentId);
+  const courses = useAppSelector((state) => state.student.courses);
 
-export default Courses
+  useEffect(() => {
+    doGetCourses(studentId).then((res) => {
+      if (res?.status === 200) {
+        dispatch(studentActions.getCourses(res?.data.courses));
+      }
+    });
+  }, []);
+  return (
+    <div style={styles.root}>
+      <Grid container>
+        <Grid item xs={12} sx={{ marginBottom: !matches ? "20px" : "10px" }}>
+          <Typography sx={styles.title}>Courses</Typography>
+          <Divider />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography sx={{ marginBottom: "10px" }}>
+            Even Semester 2022
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          container
+          xs={12}
+          spacing={2}
+          sx={{
+            marginBottom: !matches ? 0 : "20px",
+          }}
+        >
+          {courses.map((course) => (
+            <Grid item xs={4}>
+              <Paper sx={styles.paper}>
+                <Typography>{course.courseName}</Typography>
+                <Typography>{course.courseId}</Typography>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Courses;
--- src/containers/Dashboard/ApiServiceDashboard.ts
+++ src/containers/Dashboard/ApiServiceDashboard.ts
@@ -4,10 +4,51 @@ const headersSetting = {
   "Content-Type": "application/json",
 };
 
-export const doGetStudentData = async () => {
+const baseUrl = "http://localhost:4646/student"
+
+export const doGetProfileInfo = async (studentId: number) => {
   try {
     const result = await Axios({
-      url: `http://localhost:4646/student/getStudentData`,
+      url: `${baseUrl}/getProfileInfo/${6051901232}`,
+      method: "GET",
+      headers: headersSetting,
+    });
+    return result;
+  } catch (err) {
+    alert(`Terjadi Kesalahan: ! \n${err}`);
+  }
+};
+
+export const doGetInfoCard = async (studentId: number) => {
+  try {
+    const result = await Axios({
+      url: `${baseUrl}/getInfoCard/${studentId}`,
+      method: "GET",
+      headers: headersSetting,
+    });
+    return result;
+  } catch (err) {
+    alert(`Terjadi Kesalahan: ! \n${err}`);
+  }
+};
+
+export const doGetStudentGrades = async (studentId: number) => {
+  try {
+    const result = await Axios({
+      url: `${baseUrl}/getStudentGrades/${studentId}`,
+      method: "GET",
+      headers: headersSetting,
+    });
+    return result;
+  } catch (err) {
+    alert(`Terjadi Kesalahan: ! \n${err}`);
+  }
+};
+
+export const doGetCourses = async (studentId: number) => {
+  try {
+    const result = await Axios({
+      url: `${baseUrl}/getCourses/${studentId}`,
       method: "GET",
       headers: headersSetting,
     });
